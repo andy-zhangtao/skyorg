f6c8cf9fe21030a807eab6391eb36da1812ac8a5
andy <ztao8607@gmail.com>
2018-09-04 14:06:55 +0800

    Add help functions
    Signed-off-by: andy <ztao8607@gmail.com>

--- pool/conn.go
+++ pool/conn.go
@@ -1,1 +1,59 @@
 package pool
+
+import (
+	"net"
+)
+
+type connPool struct {
+	Conn   net.Conn
+	Use    bool
+}
+
+type ConnPool struct {
+	pools []*connPool
+}
+
+func InitPool(num int) *ConnPool {
+	return &ConnPool{
+		pools: make([]*connPool, num),
+	}
+}
+
+func (this *ConnPool) AddConn(conn net.Conn) {
+	for i, c := range this.pools {
+		if c == nil {
+			this.pools[i] = &connPool{
+				Conn:   conn,
+				Use:    false,
+			}
+			break
+		}
+	}
+}
+
+func (this *ConnPool) Cap() int {
+	i := 0
+
+	for _, c := range this.pools {
+		if c != nil {
+			i++
+		}
+	}
+
+	return i
+}
+
+func (this *ConnPool) NextConn() (int, *net.Conn) {
+	for i, c := range this.pools {
+		if c != nil && !c.Use {
+			c.Use = true
+			return i, &c.Conn
+		}
+	}
+
+	return 0, nil
+}
+
+func (this *ConnPool) Destory(i int) {
+	this.pools[i] = nil
+}
--- tools/request.go
+++ tools/request.go
@@ -1,1 +1,27 @@
 package tools
+
+import (
+	"fmt"
+	"net"
+)
+
+func HandleRequest(src net.Conn, dest net.Conn, errMsg chan bool) {
+	buf := make([]byte, 0xffff)
+	for {
+		n, err := src.Read(buf)
+		if err != nil {
+			fmt.Println("User Closed")
+			break
+		}
+
+		b := buf[:n]
+
+		n, err = dest.Write(b)
+		if err != nil {
+			fmt.Printf("dest err %v", err)
+			break
+		}
+	}
+
+	errMsg <- true
+}
